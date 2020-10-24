3bf2c073125ee80139dd002b0a4f045958d3e960
edo <ahmad.ridwan@alfadigital.id>
2020-10-24 15:51:31 +0700

    first commit;

---
+++ TesterNgoding.java
@@ -0,0 +1,13 @@
+public class TesterNgoding{//ini class
+
+
+
+public static void main(String... argument){//ini function
+String nama="Ferry";
+int rekA=1_000_000;
+int rekB=1_000_000;
+int rekC=(rekA+rekB);
+nama="3000";
+System.out.println(nama+" Rp."+rekC);
+}
+}
