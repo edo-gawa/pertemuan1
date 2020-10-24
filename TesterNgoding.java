10591f5b19e34298ca3177bcf8f5c9101c269edb
edo <ahmad.ridwan@alfadigital.id>
2020-10-24 15:54:15 +0700

    update String Rp to RP

--- TesterNgoding.java
+++ TesterNgoding.java
@@ -7,7 +7,6 @@ String nama="Ferry";
 int rekA=1_000_000;
 int rekB=1_000_000;
 int rekC=(rekA+rekB);
-nama="3000";
-System.out.println(nama+" Rp."+rekC);
+System.out.println(nama+" RP."+rekC);
 }
 }
